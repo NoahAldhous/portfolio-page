b00089503fe7c6b17ce6774452b450271d821b89
NoahAldhous <naldhous@hotmail.co.uk>
2022-07-18 12:30:23 +0100

    basic shape of navbar and header added

---
+++ plan.js
@@ -0,0 +1,20 @@
+/* Component structure
+
+App- a div that containts all other elements.
+    Header- a div that contains the homepage button and email. width of scren Constant on screen (maybe disappears on downward scroll?)
+        Home button- a button component that returns the user to the top of the page.
+        Email- an anchor tags that allow users to send me an email.
+    Main- The body of the app. Made up of Navbar on the left, content in the center, and line on the right.
+        Navbar-Container- a div that takes up the height of the screen and stays with scroll.
+            Navbar- a container of buttons that expands on hover.
+                Navbar buttons- button components that take the user to different parts of the website.
+        Content-Container- a div that hold the separate content 'pages'. Scrollable.
+            Content-page
+                Home-page
+                About-me-page
+                Portfolio-page 
+                Experience-page
+                Contact-me-page
+
+
+ */
---
+++ src/components/App/App.js
@@ -0,0 +1,51 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Header">Hello</div>
+      <div className="Main">
+        <div className="Navbar-container">
+          <div className="Navbar"></div>
+        </div>
+        <div className="Content-container">
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+          <h2>Noah</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
