951e61c7cd571726fdb829e4c8bb92e580cc2166
NoahAldhous <naldhous@hotmail.co.uk>
2022-07-18 17:38:55 +0100

    refactored navbar and header into separate componentfolders

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import Header from '../Header/Header';
-import Navbutton from '../Navbutton/Navbutton';
+import Navbar from '../Navbar/Navbar';
 import './App.css';
 
 function App() {
@@ -8,17 +8,7 @@ function App() {
       <Header/>
       <div className="Main">
         <div className="Navbar-container">
-          <div className="Navbar">
-            <Navbutton buttonText = "AM"/>
-            <Navbutton buttonText = "PF"/>
-            <Navbutton buttonText = "EX"/>
-            <Navbutton buttonText = "CI"/>
-            <Navbutton buttonText = "LI"/>
-            <Navbutton buttonText = "TW"/>
-            <Navbutton buttonText = "GH"/>
-            <Navbutton buttonText = "IG"/>
-          </div>
-          
+          <Navbar/>
         </div>
         <div className="Content-container">
           <h2>Text</h2>
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import './Navbar.css'
+import Navbutton from '../Navbutton/Navbutton'
+
+export default function Navbar(){
+    return  <div className="Navbar">
+    <Navbutton buttonText = "AM"/>
+    <Navbutton buttonText = "PF"/>
+    <Navbutton buttonText = "EX"/>
+    <Navbutton buttonText = "CI"/>
+    <Navbutton buttonText = "LI"/>
+    <Navbutton buttonText = "TW"/>
+    <Navbutton buttonText = "GH"/>
+    <Navbutton buttonText = "IG"/>
+  </div>
+  
+}
