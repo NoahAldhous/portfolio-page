34b738d6c4b97df7c480c28afea997597c60cf66
NoahAldhous <naldhous@hotmail.co.uk>
2022-07-29 23:29:05 +0100

    about page done

--- src/components/Aboutmepage/Aboutmepage.js
+++ src/components/Aboutmepage/Aboutmepage.js
@@ -29,8 +29,28 @@ export default function Aboutmepage(){
         <h2 className= "Subheading">ABOUT ME</h2>
         <section className= "Aboutme-container">
             <section className="Abouttext-container">
-                <p className="Abouttext">Its me</p>
-                <p className="Skillstext"> currently working with</p>
+                <p className="Abouttext">
+                    Hi there! My name is Noah. <br/> 
+                    About 6 months ago I was working in 
+                    hospitality, and hadn't written a single line of code... <br/>
+                    After 16 intense but incredible weeks on the SoC Bootcamp,
+                    I've stopped being a server, and started building them!
+                </p>
+                <section className="Skills-container">
+                <p className="Skillstext"> Currently Working With</p>
+                <section className="Tech-list-container">
+                    <section className="Tech-list">
+                            <p className="Tech">React JS</p>
+                            <p className="Tech">AWS Amplify</p>
+                            <p className="Tech">postgreSQL</p>
+                    </section>
+                    <section className="Tech-list">
+                        <p className="Tech">Chakra UI</p>
+                        <p className="Tech">Express JS</p>
+                        <p className="Tech">Typescript</p>
+                    </section>
+                    </section>
+                </section>
             </section>
             <section className= "Profile-card">
                 <img className="Profile-picture" src={image} alt="Noah Aldhous, wearing a shirt"/>
--- src/components/Semicircle/Semicircle.js
+++ src/components/Semicircle/Semicircle.js
@@ -3,5 +3,6 @@ import './Semicircle.css'
 export default function Semicircle(){
   
 
-    return <div className = "semiCircle"></div>
+    return <div className = "semiCircle">
+    </div>
 }
