32b2b5a53af4e4ba2f2bec2087c509d04cec8e7c
NoahAldhous <naldhous@hotmail.co.uk>
2022-08-23 15:02:47 +0100

    added portfolio resizing

--- src/components/PortfolioPage/PortfolioCard/PortfolioCard.js
+++ src/components/PortfolioPage/PortfolioCard/PortfolioCard.js
@@ -5,15 +5,17 @@ import { AiFillGithub, AiOutlineLink } from "react-icons/ai";
 export default function PortfolioCard({githubLink , deployedLink, title, text, image }){
     return <div className="Portfolio-card">
                 <div className="card-overlay">
-                    <a className="Icon-button" href = {githubLink} target='_blank' rel='noreferrer'>
-                        <AiFillGithub className="Github-icon" size={50}/>
-                    </a>
+                    <section className = "icon-container">
+                        <a className="Icon-button" href = {githubLink} target='_blank' rel='noreferrer'>
+                            <AiFillGithub className="Github-icon" size={50}/>
+                        </a>
+                        <a className="Icon-button" href = {deployedLink} target='_blank' rel='noreferrer'>
+                            <AiOutlineLink className="Link-icon" size={50}/>
+                        </a>
+                    </section>
                     {title}     
                     <br/>
                     {text}
-                    <a className="Icon-button" href = {deployedLink} target='_blank' rel='noreferrer'>
-                        <AiOutlineLink className="Link-icon" size={50}/>
-                    </a>
                 </div>
                 <img className= "Portfolio-image" src= {image} alt= "noah"/>
             </div>
