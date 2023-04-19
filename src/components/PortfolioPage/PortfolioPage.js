9d5baa65a298e9ec324620d7a61ae3b4920a7464
NoahAldhous <naldhous@hotmail.co.uk>
2023-04-19 18:48:26 +0100

    added new portfolio image for myToDoList app in portfolio page.

--- src/components/PortfolioPage/PortfolioPage.js
+++ src/components/PortfolioPage/PortfolioPage.js
@@ -6,6 +6,7 @@ import pokebrawlz from './PortfolioImages/pokebrawlz.png';
 import portfoliopage from './PortfolioImages/portfoliopage.png';
 import digitalNoah from '../Aboutmepage/images/digitalnoah.png';
 import splitTheBill from './PortfolioImages/splitTheBill.png';
+import myToDoList from './PortfolioImages/myToDoList.png'
 
 export default function PortfolioPage(){
    return <div className="PortfolioPage">
@@ -16,7 +17,7 @@ export default function PortfolioPage(){
                 <PortfolioCard githubLink = "https://github.com/NoahAldhous/pokemonfetchapp" deployedLink = "https://pokebrawlz.netlify.app/" title = "POKEBRAWLZ" text = "a React.js app using the PokeAPI to find random pokemon" image = {pokebrawlz}/>
                 <PortfolioCard githubLink = "https://github.com/NoahAldhous/portfolio-page" deployedLink = "https://www.noahaldhous.com/" title = "THIS SITE" text = "oooh so meta! Built with React.js" image = {portfoliopage}/>
                 <PortfolioCard githubLink = "https://github.com/NoahAldhous/split-the-bill" deployedLink = "https://bill-calculator-ecru.vercel.app/" title = "SPLIT THE BILL" text = "A Vue app to help people split their restaurant bills. Bult with a mobile-first design." image = {splitTheBill}/>
-                <PortfolioCard githubLink = "https://github.com/NoahAldhous" deployedLink = "https://jungle-sum.netlify.app/" text = "Coming Soon!" image = {digitalNoah}/>
+                <PortfolioCard githubLink = "https://github.com/NoahAldhous" deployedLink = "https://jungle-sum.netlify.app/" text = "Coming Soon!" image = {myToDoList}/>
             </section>
         </div>
 }
