9cbc8553dc932b88ae3a1d678374baf61eda1a90
NoahAldhous <naldhous@hotmail.co.uk>
2023-04-26 14:32:37 +0100

    changed the order of items in my portfolio

--- src/components/PortfolioPage/PortfolioPage.js
+++ src/components/PortfolioPage/PortfolioPage.js
@@ -11,12 +11,12 @@ export default function PortfolioPage(){
    return <div className="PortfolioPage">
             <h2 id="PortfolioPageSubheading" className="Subheading">MY PORTFOLIO</h2>
             <section className="Portfolio-grid">
-                <PortfolioCard githubLink = "https://github.com/NoahAldhous" deployedLink = "https://jungle-sum.netlify.app/" title = "JUNGLE SUMS" text = "an educational app for homeschooled children built with ReactJS, Auth0 and PostgreSQL." image = {jungleSums}/>
                 <PortfolioCard githubLink = "https://github.com/NoahAldhous/just-my-type" deployedLink = "https://visionary-klepon-ded6d3.netlify.app/" title = "JUST MY TYPE" text = "a simple game designed to test your typing skill. Built with ReactJS." image = {justMyType}/>
                 <PortfolioCard githubLink = "https://github.com/NoahAldhous/pokemonfetchapp" deployedLink = "https://pokebrawlz.netlify.app/" title = "POKEBRAWLZ" text = "a React.js app using the PokeAPI to find random pokemon." image = {pokebrawlz}/>
-                <PortfolioCard githubLink = "https://github.com/NoahAldhous/portfolio-page" deployedLink = "https://www.noahaldhous.com/" title = "THIS SITE" text = "oooh so meta! Built with React.js" image = {portfoliopage}/>
                 <PortfolioCard githubLink = "https://github.com/NoahAldhous/split-the-bill" deployedLink = "https://bill-calculator-ecru.vercel.app/" title = "SPLIT THE BILL" text = "A Vue app to help people split their restaurant bills. Bult with a mobile-first design." image = {splitTheBill}/>
                 <PortfolioCard githubLink = "https://github.com/NoahAldhous/to-do-list-frontend" deployedLink = "https://storied-unicorn-46d331.netlify.app/" title = "MY TO-DO LIST" text = "A full-stack MERN to-do list app. Written with Typescript and Tailwind CSS." image = {myToDoList}/>
+                <PortfolioCard githubLink = "https://github.com/NoahAldhous/portfolio-page" deployedLink = "https://www.noahaldhous.com/" title = "THIS SITE" text = "oooh so meta! Built with React.js" image = {portfoliopage}/>
+                <PortfolioCard githubLink = "https://github.com/NoahAldhous" deployedLink = "https://jungle-sum.netlify.app/" title = "JUNGLE SUMS" text = "an educational app for homeschooled children built with ReactJS, Auth0 and PostgreSQL." image = {jungleSums}/>
             </section>
         </div>
 }
